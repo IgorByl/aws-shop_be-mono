c4293b370286193760547dc9b579047a9fff9369
Ihar Bulaty <Ihar_Bulaty@epam.com>
2020-11-15 13:35:43 +0300

    Task_5_add_s3_import/upload

---
+++ import-service/src/import-products.ts
@@ -0,0 +1,30 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import "source-map-support/register";
+import { errorHandler } from "../utils";
+import * as AWS from "aws-sdk";
+import * as util from "util";
+
+export const importProductsFile: APIGatewayProxyHandler = errorHandler(
+  async (event) => {
+    const s3 = new AWS.S3({ maxRetries: 0, apiVersion: "2012-11-05" }),
+      getSignedUrl = util.promisify(s3.getSignedUrl.bind(s3));
+
+    console.log("Lambda importProductsFile was called with event: ", event);
+
+    const { name } = event.queryStringParameters;
+    console.log("Lambda importProductsFile requested file .csv: ", name);
+
+    const { BUCKET_NAME } = process.env;
+    const putParams = {
+      Bucket: BUCKET_NAME,
+      Key: `uploaded/${name}.csv`,
+      Expires: 120,
+      ContentType: "text/csv",
+    };
+
+    const putUrl = await getSignedUrl("putObject", putParams);
+    console.log("Lambda importProductsFile returns putObject url: ", putUrl);
+
+    return putUrl;
+  }
+);
---
+++ import-service/src/parse-products.ts
@@ -0,0 +1,63 @@
+import { S3Handler, S3Event } from "aws-lambda";
+import "source-map-support/register";
+import * as AWS from "aws-sdk";
+import * as csv from "csv-parser";
+import { ProductImportBucketFolders } from "../constants";
+
+export const importFileParser: S3Handler = async (event: S3Event) => {
+  const s3 = new AWS.S3({ maxRetries: 0, apiVersion: "2012-11-05" });
+  console.log("Lambda importFileParser was called with event: ", event);
+
+  for (const record of event.Records) {
+    console.log("Lambda importFileParser handle record: ", record);
+
+    const srcBucket = record.s3.bucket.name;
+    console.log("Lambda importFileParser s3 Bucket name:", srcBucket);
+
+    const srcKey = record.s3.object.key;
+    console.log("Lambda importFileParser s3 Bucket key:", srcKey);
+
+    try {
+      const s3Object = await s3.getObject({
+        Bucket: srcBucket,
+        Key: srcKey,
+      });
+      console.log("Lambda importFileParser selected object: ", s3Object);
+
+      s3Object
+        .createReadStream()
+        .pipe(csv())
+        .on("data", (data) => console.log(data));
+    } catch (error) {
+      console.log("Lambda importFileParser parsing stream error: ", error);
+    }
+
+    try {
+      await s3
+        .copyObject({
+          Bucket: srcBucket,
+          CopySource: `${srcBucket}/${srcKey}`,
+          Key: srcKey.replace(
+            ProductImportBucketFolders.UPLOADED,
+            ProductImportBucketFolders.PARSED
+          ),
+        })
+        .promise();
+
+      await s3
+        .deleteObject({
+          Bucket: srcBucket,
+          Key: srcKey,
+        })
+        .promise();
+
+      console.log(
+        `Lambda importFileParser transfer for the folder ${
+          srcKey.split("/")[1]
+        } was done.`
+      );
+    } catch (error) {
+      console.log("Lambda importFileParser moving files error: ", error);
+    }
+  }
+};
